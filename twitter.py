46336d212cef77efa802615f774b2d954242f16d
kevin <scu.bjwu@gmail.com>
2015-09-17 17:35:24 -0500

    fetch infor with multiple thread for rate limitation

--- twitter.py
+++ twitter.py
@@ -13,28 +13,18 @@ from bson.codec_options import CodecOptions
 import threading
 
 class twitter_T:
-	ckey = ""
-	csecret = ""
-	atoken = ""
-	asecret = ""
-	t_api = None
-
 	def __init__(self, name, ckey, csecret, atoken, asecret):
 		self.name = name
 		
-		if twitter_T.t_api is not None:
-			print "already init..."
-			return
-
-		twitter_T.ckey = ckey
-		twitter_T.csecret = csecret
-		twitter_T.atoken = atoken
-		twitter_T.asecret = asecret
+		self.ckey = ckey
+		self.csecret = csecret
+		self.atoken = atoken
+		self.asecret = asecret
 		
 		#init the api handler
 		auth = tweepy.OAuthHandler(self.ckey, self.csecret)
 		auth.set_access_token(self.atoken, self.asecret)
-		twitter_T.t_api = tweepy.API(auth_handler=auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+		self.t_api = tweepy.API(auth_handler=auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 	#def __del__(self):
 	#	twitter_T.twitter_cnt -= 1
@@ -51,10 +41,10 @@ class twitter_T:
 			try:
 			    page = tmp.next()
 			    lst.extend(page)
-			except tweepy.TweepError:
-                            print "!ERROR:when extract friends list: ", tweepy.TweepError
-			    time.sleep(60*15)
-			    continue
+                        #the user not auth us to extract his relationship or he does not follow anyone....
+			except tweepy.TweepError as e:
+                            print "Error in followers_lst: ", e.reason
+                            return [] 
 			except StopIteration:
 			    break
 		
@@ -67,20 +57,57 @@ class twitter_T:
 			try:
 			    page = tmp.next()
 			    lst.extend(page)
-			except tweepy.TweepError:
-                            print "!ERROR:when extract friends list: ", tweepy.TweepError
-			    time.sleep(60*15)
-			    continue
+			except tweepy.TweepError as e:
+                            print "Error in friends_lst: ", e.reason
+                            return []
 			except StopIteration:
 			    break
 		return lst
 
 #=================================global infor=======================================#
-my_ckey = "veCUf9FagS16QmG0PWkXRVMLa"
-my_csecret = "iX6GSn8NG6GeBGDT7OUKYmOmlnCGUlj8cOQn91ebHjXEwtAybY"
-my_atoken = "2509388425-H2EXibllWS0jfp3N0ShOiPJxnGSAuet5EOM7b25"
-my_asecret = "DnNO0S1DHYsrBofYZGdJlbWpxctTfNS2mjtynyxpfXyQp"
-my_twitter = twitter_T('t_user', my_ckey, my_csecret, my_atoken, my_asecret)
+my_twitter = []
+
+ckey0 = "OVJ62utfPINH1tjJlX3gACqAo"
+csecret0 = "W0zw7Y4zWATZwRiBKeVLsHNTHwrbui1IekH9f5vqInRXEHyutM"
+atoken0 = "3688208117-QDc0CWpLe24EETnKDVZ28G6pCv7Md53A50Bb5Uk"
+asecret0 = "3MG2tAHiraigWwX5U3nq0ECDqDFMjuRrEm66DpD7rLwyj"
+twitter0 = twitter_T('t_user0', ckey0, csecret0, atoken0, asecret0)
+my_twitter.append(twitter0)
+
+ckey1 = "donoBSHOjnHt4YPG3avSmUvro"
+csecret1 = "RARzNyiPPkg8U6DBXofQM4sbrERiz7xPFii9bJXi7RVm79IFpO"
+atoken1 = "2509388425-pZiEKSsbY4rIlCVr0Zl6N6LU5ICRdz283ACu9is"
+asecret1 = "7EGL5FXVkwZICn8sIjxxkTBCOSs7oEvLJTAvpCdsTvDcZ"
+twitter1 = twitter_T('t_user1', ckey1, csecret1, atoken1, asecret1)
+my_twitter.append(twitter1)
+
+ckey2 = "7nWywVmFealeMvswit3q8gmK0"
+csecret2 = "dGCzEqccsMadjlJaF1wdLXYVarGfUmYOGdsAlT4d2Fng3uRXAh"
+atoken2 = "3688208117-wgKdmVnCFuMkIJT5U8NzUKKuBhpX6G5xy45zFBW"
+asecret2 = "Twytg9TkwByuXU2YllAwFWcZKRO7xbAMKY8MpgSa6bUKu"
+twitter2 = twitter_T('t_user2', ckey2, csecret2, atoken2, asecret2)
+my_twitter.append(twitter2)
+
+ckey3 = "veCUf9FagS16QmG0PWkXRVMLa"
+csecret3 = "iX6GSn8NG6GeBGDT7OUKYmOmlnCGUlj8cOQn91ebHjXEwtAybY"
+atoken3 = "2509388425-H2EXibllWS0jfp3N0ShOiPJxnGSAuet5EOM7b25"
+asecret3 = "DnNO0S1DHYsrBofYZGdJlbWpxctTfNS2mjtynyxpfXyQp"
+twitter3 = twitter_T('t_user3', ckey3, csecret3, atoken3, asecret3)
+my_twitter.append(twitter3)
+
+ckey4 = "S5Rga6CAX6KyiHki7UsxCqzxj"
+csecret4 = "j7NBQHHSDGUgLif795UiBq6cpCFiOVjeDbn4D6EArtolKXgpod"
+atoken4 = "2509388425-ijsseTtnXXnt6SqGy0PhVTGOZgjFkA9GCFX4dY6"
+asecret4 = "qXGpDyGYl4W5VNLfjKvrV47vfAHx41p0Xk143ir6EAt1j"
+twitter4 = twitter_T('t_user4', ckey4, csecret4, atoken4, asecret4)
+my_twitter.append(twitter4)
+
+ckey5 = "MkzQu5X3j1WSLGnggcmq7DYzi"
+csecret5 = "5zzM5rISHOO8H6q1HxqrcOzpXjHoap0ax2mgQk6JcNZtMvDpDU"
+atoken5 = "3688208117-2zXF1SuaXAh7uhoDdPcQGCC73EE2woEjhLxFp76"
+asecret5 = "9oMNO5ySUC5IXw1wKqK4RvmUrtJ48KdbYoni9I3dKFqx6"
+twitter5 = twitter_T('t_user5', ckey5, csecret5, atoken5, asecret5)
+my_twitter.append(twitter5)
 
 client_user = MongoClient()
 db_user = pymongo.database.Database(client_user, "test")
@@ -89,7 +116,7 @@ collection_user.create_index("user_id")
 
 db_lock = threading.Lock()
 #======================================================================================#
-def process_document(document):
+def process_document(document, twitter_h):
     user_info = document['user']
 
     user_id = json.loads(user_info['id_str'])
@@ -101,8 +128,8 @@ def process_document(document):
     if res is not None:
         return
 
-    lst_fo = my_twitter.followers_lst(user_id)
-    lst_fr = my_twitter.friends_lst(user_id)
+    lst_fo = twitter_h.followers_lst(user_id)
+    lst_fr = twitter_h.friends_lst(user_id)
 
     new_doc = {}
     new_doc['user_id'] = user_id
@@ -115,45 +142,45 @@ def process_document(document):
             print "insert a new doc..."
             collection_user.insert_one(new_doc)
 
-def process_cursor(cursor):
+def process_cursor(cursor, twitter_h):
     for document in cursor:
-        process_document(document)
+        process_document(document, twitter_h)
 
 def main():
-    my_twitter.twitter_DB('test', 'tweet_NYC')
-    cursors = my_twitter.collection.parallel_scan(4)
-    threads = [
-            threading.Thread(target=process_cursor, args=(cursor,))
-            for cursor in cursors]
+    tweet_client = MongoClient()
+    tweet_db = pymongo.database.Database(tweet_client, "test")
+    tweet_collection = pymongo.collection.Collection(tweet_db, "tweet_NYC")
+
+    cursors = tweet_collection.parallel_scan(6)
+    threads = []
+    for i in range(6):
+        threads.append(threading.Thread(target=process_cursor, args=(cursors[i], my_twitter[i],)))
 
     print "Start..."
-
     for thread in threads:
         thread.start()
 
     for thread in threads:
         thread.join()
-
     print "Done!"
 
 if __name__ == '__main__':
     main()
 
 """
-my_twitter.twitter_DB('test', 'tweet_NYC')
-res = my_twitter.collection.find_one({"id_str":"636433425336037376"})
+my_twitter[3].twitter_DB('test', 'tweet_NYC')
+res = my_twitter[3].collection.find_one({"id_str":"636433425336037376"})
 #extract and construct the followers and friends infor of this user
 user_info = res['user']
 user_id = json.loads(user_info['id_str'])
-lst_fo = my_twitter.followers_lst(user_id)
-lst_fr = my_twitter.friends_lst(user_id)
+lst_fo = my_twitter[3].followers_lst(user_id)
+lst_fr = my_twitter[3].friends_lst(user_id)
 
 user_test = {}
 user_test['user_id'] = user_id
 user_test['friends_id'] = lst_fr
 user_test['followers_id'] = lst_fo
 print user_test
-
 #insert this user info if it is new
 #res = collection_user.find_one({'user_id':user_id})
 #if res is None:
