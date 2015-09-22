a9e0f52b2d6e7d878828497f3ad04ec2183ebc7d
kevin <scu.bjwu@gmail.com>
2015-09-22 10:57:47 -0500

    merge the splitted collections

---
+++ collection_merge_1.js
@@ -0,0 +1,24 @@
+//usage:
+//mongo --eval "var DB_name='xx', col_target_name='xx', col_src_name='xx'"
+conn = new Mongo();
+
+//put the database name below
+db = conn.getDB(DB_name);
+var count = 0;
+
+//put the collection name that you want to merge to
+var col_target = db.getCollection(col_target_name)
+count = col_target.count();
+print(count);
+
+col_target.createIndex({user_id : 1}, unique=true);
+
+//put the collection name that you want to merge from
+var col_src = db.getCollection(col_src_name)
+cursor = col_src.find();
+while (cursor.hasNext()) {
+	col_target.insert(cursor.next());
+}
+
+count = col_target.count();
+print(count)
