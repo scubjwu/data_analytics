932f38b9940eeabc3511c36d9e2ecc15df20d514
kevin <scu.bjwu@gmail.com>
2015-09-26 12:11:49 -0500

    add script for tweet location update

---
+++ collection_loc_update.js
@@ -0,0 +1,18 @@
+//usage:
+//mongo --eval "var DB_name='xx', col_name='xx'"
+var conn = new Mongo();
+
+//put the database name below
+var db = conn.getDB(DB_name);
+
+//put the collection name that you want to operate
+var col = db.getCollection(col_name);
+
+var cursor = col.find();
+
+while(cursor.hasNext()) {
+	var doc = cursor.next();
+	var coor = doc.place.bounding_box.coordinates[0];
+	col.update({_id : doc._id}, {$set : {"loc_point" : [(coor[0][0] + coor[2][0])/2, (coor[0][1] + coor[2][1])/2]}});
+}
+
